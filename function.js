ad679bad358ce5b1463d3a1e1b9cd5a811dbeb14
Arman Mir <mirarman8583@gmail.com>
2025-08-11 18:32:03 +0600

    function and parameter

---
+++ function.js
@@ -0,0 +1,34 @@
+const number = 45;
+
+
+//function declare................>>>>
+function fanOffKor(){
+    console.log('bosa theke uthe dara');
+    console.log('walk towards the switch');
+    console.log('click the switch to off of the fan');
+}
+
+
+//function keyword (){
+//
+//  }
+function brushTeeth(){
+    console.log('pick up the brush');
+    console.log('add paste');
+    console.log('two mins ghoso and ghoso and fena felo');
+}
+
+//call the function 
+fanOffKor();
+
+
+//call the function 
+brushTeeth();
+
+console.log('------------------ ');
+console.log('------------------ ');
+console.log('------------------ ');
+console.log('------------------ ');
+console.log('------------------ ');
+
+brushTeeth();
---
+++ parameter.js
@@ -0,0 +1,47 @@
+function vatkhao(){
+    console.log('HandWaash kore aso');
+    console.log('Boso');
+    console.log('Plate e khabar dabar naooo');
+    console.log('gopat gopat kore khao');
+}
+
+// vatkhao();
+
+
+function square (number){
+    console.log('Value of the number Parameter', number);
+    const borgo = number * number;
+    console.log('Square of the given number is',borgo);
+
+
+}
+
+square(156444);
+console.log('------------------');
+square(122);
+console.log('-------------------');
+square(4064561);
+console.log('-------------------------------');
+
+
+
+function add(num1 , num2){
+    const sum = num1 + num2;
+    console.log(sum);
+}
+
+add(10 , 45644);
+console.log('-------------------------------');
+
+add(54661361, 54646);
+
+
+function addAll(a , b ,c ,d , e){
+    console.log('-------------------------------');
+    const total = a + b + c + d +e;
+    console.log(total);console.log('-------------------------------');
+}
+
+
+addAll(455644, 65464 , 6464646 , 6546464 ,64465465);
+addAll(4564646, 46464, 58 , 546456 , 6446);
